25a33222f66dd443f775c496614c04232df9615a
Mohammed Naji <m.naji1512@gmail.com>
2021-09-18 18:44:16 +0300

    lec1

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    // print "Main";
+    // echo "Main";
+    // print("Main");
+    // echo("Main");
+    
+
+    //echo 'This string from php code';
+    // echo '<br>';
+    //echo '<br>This string from php code';
+    //echo "<h1 style='color: red'>This string from HTML code</h1>";
+
+    ///////////////////////////
+
+    // int x = 5;
+    $x = 5;
+    $name = 'Mohammed Naji'; // string
+    $age = 27;
+    // $name = 8;
+    $check = false;
+    $y = 2.3;
+
+    // var_dump($y);
+
+    // echo $name;
+
+    // echo "Welcome " . $name . ", your age is " . $age . " we hope to have a nice time with us";
+    // echo 'Welcome ' . $name . ', your age is ' . $age . ' we hope to have a nice time with us';
+    // echo "Welcome $name, your age is $age we hope to have a nice time with us";
+    // echo 'Welcome $name, your age is $age we hope to have a nice time with us';
+
+    echo "<p>Welcome <b style='color: red'>$name</b>, your age is <b style='color: blue'>$age</b> we hope to have a nice time with us</p>";
+?>
+
+<p>Welcome <b style="color: red;"><?php echo $name ?></b>, your age is <b style="color: blue;"><?php echo $age ?></b> we hope to have a nice time with us</p>
+
+<!-- <h1 style="color: red">This string from HTML code</h1> -->
+
